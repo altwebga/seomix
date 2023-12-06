eff9ef82dae20533a6bc1c2f63e430ef08bc7269
Konstantin <altwebga@gmail.com>
2023-12-06 21:21:40 +0700

    Add: hero component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,51 +5,13 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
+import Hero from "@/components/hero";
 
 export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Разработка и&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>продвижение сайтов&nbsp;</h1>
-        <br />
-        <h1 className={title()}>
-          Мобильных приложений
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Адекватный креатив и технологичные решения.
-        </h2>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          href={siteConfig.links.docs}
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-
-      <div className="mt-8">
-        <Snippet hideSymbol hideCopyButton variant="flat">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
-    </section>
-  );
+  return(
+    <>
+    <Hero />
+    </>
+  ) 
+  
 }
