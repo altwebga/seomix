87f274ca5515cf587d464523b71fc0fa18e8d86d
Konstantin <altwebga@gmail.com>
2024-08-16 21:42:16 +0700

    fix Layout

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,7 @@
-import { title } from "@/components/primitives";
-
 export default function ContactPage() {
   return (
     <div>
-      <h1 className={title()}>Docs</h1>
+      <h1>Контакты</h1>
     </div>
   );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,9 +1,7 @@
-import { title } from "@/components/primitives";
-
 export default function Page() {
   return (
     <div>
-      <h1 className={title()}>Pricing</h1>
+      <h1>Мои работы</h1>
     </div>
   );
 }
