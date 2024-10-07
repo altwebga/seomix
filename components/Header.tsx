a970392ef8101426a23b3941cdcedcb95b452d03
Konstantin <altwebga@gmail.com>
2024-10-07 17:20:12 +0700

    Add: logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,16 @@
 import { HeaderNav } from "./HeaderNav";
+import { Logo } from "./Logo";
 import { MobileNav } from "./MobileNav";
 import { ThemeToggle } from "./ThemeToggle";
+import Link from "next/link";
 
 export function Header() {
   return (
     <header className="sticky top-0 flex w-full items-center justify-between border-b bg-background px-4 py-2 shadow-sm">
-      <div>logo</div>
+      <Link href="/" className="flex items-center gap-2">
+        <Logo />
+        <h4>seomix.</h4>
+      </Link>
       <HeaderNav />
       <div className="flex items-center gap-4">
         <MobileNav />
