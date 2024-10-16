0691134f3fbde858e7d26cc8e24fc20dff3b6747
Konstantin <altwebga@gmail.com>
2024-10-16 17:17:09 +0700

    new project

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-import nextMDX from "@next/mdx";
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    rehypePlugins: [],
-  },
-});
-
-const nextConfig = withMDX({
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-});
+const nextConfig = {};
 
 export default nextConfig;
