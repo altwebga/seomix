5f660cfdeae797d5350de550ffc458c7c1a34937
Konstantin <altwebga@gmail.com>
2024-10-23 22:12:25 +0700

    new component

--- components/ServicesHome.tsx
+++ components/ServicesHome.tsx
@@ -13,7 +13,7 @@ export function ServicesHome() {
   const [posts, setPosts] = useState<Post[] | null>(null);
 
   useEffect(() => {
-    getPosts(3, 12).then((posts) => {
+    getPosts(4, 12).then((posts) => {
       setPosts(posts);
     });
   });
@@ -21,14 +21,30 @@ export function ServicesHome() {
     return <div>Загружаю...</div>;
   }
   return (
-    <section className="mt-12">
-      <h2>Какие услуги я предлагаю</h2>
-      <Accordion type="single" collapsible className="w-full">
+    <section className="mt-12 flex flex-col md:flex-row gap-8 justify-between">
+      <div className="flex-1">
+        <h2>Какие услуги я предлагаю</h2>
+        <p>
+          Работайте с единым подрядчиком для всех Ваших интернет-активностей.
+          Когда ответственность за сайт, продвижение, рекламу и социальные сети
+          сосредоточена в одних руках, управление и результаты становятся
+          максимально простыми и прозрачными
+        </p>
+      </div>
+      <Accordion type="single" collapsible className="w-full flex-1">
         {posts.map((post: Post) => (
           <AccordionItem value={`item-${post.id}`} key={post.id}>
-            <AccordionTrigger>{post.title.rendered}</AccordionTrigger>
+            <AccordionTrigger className="text-lg">
+              <div className="flex flex-col md:flex-row justify-between items-start gap-4 w-full">
+                <p className="m-0 p-0">{post.title.rendered}</p>
+                <span className="text-green-500 mr-4">{post.acf?.price}</span>
+              </div>
+            </AccordionTrigger>
             <AccordionContent>
-              <p>{post.content.rendered}</p>
+              <div
+                dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+              />
+              <p>{post.acf?.price}</p>
             </AccordionContent>
           </AccordionItem>
         ))}
