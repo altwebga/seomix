7aa5586775a3432d9221e115b02e56ed02ba4848
Konstantin <altwebga@gmail.com>
2025-01-26 15:46:36 +0700

    Add: hero block

---
+++ components/hero.tsx
@@ -0,0 +1,7 @@
+export function Hero() {
+  return (
+    <div className="h-[80vh] flex flex-col items-center justify-center gap-4">
+      <h1 className="text-5xl font-bold">Добро пожаловать!</h1>
+    </div>
+  );
+}
