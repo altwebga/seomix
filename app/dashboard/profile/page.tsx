87a3139cc1d7c09d67cd914506127d237b3e7b1e
Konstantin <altwebga@gmail.com>
2025-02-19 12:07:21 +0700

    fix: prisma schema

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 export default function ProfilePage() {
+  const { data: session } = useSession();
   return (
     <div>
-      <h1>Profile</h1>
+      <h1>Профиль</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </div>
   );
 }
