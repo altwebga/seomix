b90d028518a86674f3b53ff4d3dd123499866c4a
Konstantin <altwebga@gmail.com>
2025-02-21 17:00:36 +0700

    Add: services card

---
+++ app/(frontend)/services/[slug]/page.tsx
@@ -0,0 +1,34 @@
+import { getPost } from "@/actions/get-posts";
+import Image from "next/image";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+  const data = await getPost(slug);
+  if (!data) {
+    return <div>Post not found</div>;
+  }
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="flex flex-col gap-4">
+          {data.description && (
+            <div dangerouslySetInnerHTML={{ __html: data.description }} />
+          )}
+          {data.price && <div>Цена: {data.price}</div>}
+        </div>
+        <Image
+          className="hidden md:block"
+          src={data.image || ""}
+          alt={data.title || ""}
+          width={500}
+          height={500}
+        />
+      </div>
+    </div>
+  );
+}
