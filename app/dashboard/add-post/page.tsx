45b4e3e620b3c696c02275dc9b8863141a54b173
Konstantin <altwebga@gmail.com>
2025-08-19 12:33:34 +0300

    Added: create post form

---
+++ app/dashboard/add-post/page.tsx
@@ -0,0 +1,10 @@
+import { CreatePostForm } from "@/components/create-post-form";
+
+export default function AddPostPage() {
+  return (
+    <div>
+      <h1>Добавит пост</h1>
+      <CreatePostForm />
+    </div>
+  );
+}
---
+++ components/create-post-form.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "./ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { PostType } from "@prisma/client";
+
+const FormSchema = z.object({
+  title: z.string().min(2, {
+    message: "Название не может быть короче 2 символов",
+  }),
+  postType: z.enum([PostType.POST, PostType.SERVICES, PostType.PORTFOLIO]),
+  content: z.string().min(2, {
+    message: "Описание не может быть короче 2 символов",
+  }),
+});
+
+export function CreatePostForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      title: "",
+      postType: "POST",
+      content: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast("You submitted the following values", {
+      description: (
+        <pre className="mt-2 w-[320px] rounded-md bg-neutral-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Название записи</FormLabel>
+              <FormControl>
+                <Input placeholder="Разработка сайтов" {...field} />
+              </FormControl>
+              <FormDescription>Как будет называться запись</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="postType"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Выберите тип поста" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="POST">Запись блога</SelectItem>
+                  <SelectItem value="SERVICES">Услугу</SelectItem>
+                  <SelectItem value="PORTFOLIO">Портфолио</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                В какой раздел будет опубликована запись
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Контент записи</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="....."
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>Контент записи можно в mdx</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Опубликовать</Button>
+      </form>
+    </Form>
+  );
+}
