6d546db2bc91ba9d77e67d0699a4cf0bb9ed912e
Konstantin <altwebga@gmail.com>
2025-10-02 18:03:05 +0300

    Добавлено: раздел "Обо мне" с изображениями дипломов

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,25 @@
+import { AboutMe } from "@/components/about-me";
+import { ModalImage } from "@/components/modal-image";
+import diploma_page_01 from "@/public/images/diploma_page_01.webp";
+import diploma_page_02 from "@/public/images/diploma_page_02.webp";
+
 export default function AboutPage() {
-  return <div>About</div>;
+  return (
+    <div className="container mx-auto px-4">
+      <h1>Обо мне</h1>
+      <AboutMe />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-self-center">
+        <ModalImage
+          thumbnail={diploma_page_01.src}
+          large={diploma_page_01.src}
+          alt="Диплом разработчика"
+        />
+        <ModalImage
+          thumbnail={diploma_page_02.src}
+          large={diploma_page_02.src}
+          alt="Диплом разработчика"
+        />
+      </div>
+    </div>
+  );
 }
