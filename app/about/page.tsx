d572e22980f3a9b19d0b2c08841c6308400509c4
Konstantin <altwebga@gmail.com>
2025-10-09 19:58:40 +0300

    Updated the "About" page to include a new team section featuring team members with images and descriptions, replacing the previous "AboutMe" component.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,24 @@
-import { AboutMe } from "@/components/about-me";
 import { ModalImage } from "@/components/modal-image";
 import diploma_page_01 from "@/public/images/diploma_page_01.webp";
 import diploma_page_02 from "@/public/images/diploma_page_02.webp";
+import teamImage1 from "@/public/images/me.jpg";
+import teamImage2 from "@/public/images/Team-2.jpg";
+import { Team } from "@/components/team";
 
 export default function AboutPage() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1>О нас</h1>
-      <AboutMe />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <Team image={teamImage1.src} title="Константин" content="Разработка" />
+        <Team
+          image={teamImage2.src}
+          title="Неля"
+          content="Контекстная реклама"
+        />
+      </div>
+
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-self-center">
         <ModalImage
           thumbnail={diploma_page_01.src}
