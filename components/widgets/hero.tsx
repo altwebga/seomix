a92cee41734c08ed422243d6398d6dea20e16b24
Konstantin <altwebga@gmail.com>
2025-11-14 14:32:54 +0300

    Remove unused AuroraBackground and BackgroundBeams components. Update Hero component to utilize GridPattern for enhanced layout and visual appeal.

--- components/widgets/hero.tsx
+++ components/widgets/hero.tsx
@@ -1,16 +1,39 @@
-import { BackgroundBeams } from "../ui/background-beams";
+import { GridPattern } from "../ui/grid-pattern";
+import { cn } from "@/lib/utils";
+
 export function Hero() {
   return (
-    <div className="h-screen">
-      <div className="container mx-auto p-4">
-        <h1>Background Beams</h1>
-        <p>
-          Welcome to the background beams component. This is a simple example of
-          how to use the BackgroundBeams component. Have fun building your next
-          project.
-        </p>
+    <div className="relative flex h-[80vh] w-full flex-col justify-center overflow-hidden">
+      <GridPattern
+        width={120}
+        height={120}
+        squares={[
+          [4, 4],
+          [5, 1],
+          [8, 2],
+          [5, 3],
+          [5, 5],
+          [10, 10],
+          [12, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+        ]}
+        className={cn(
+          "mask-x-from-70% mask-x-to-90%",
+          "inset-x-0 inset-y-[-30%] h-[200%] -skew-y-8"
+        )}
+      />
+      <div className="relative z-10 container mx-auto px-4">
+        <h1 className="flex flex-col gap-2 ">
+          Разработка и продвижение сайтов{" "}
+          <span className="text-[clamp(40px,5vw,80px)] text-red-500">
+            в Краснодаре
+          </span>
+        </h1>
       </div>
-      <BackgroundBeams />
     </div>
   );
 }
