f842f0cf220ae7be99a38831c08bd1b98a5d2a83
Konstantin <altwebga@gmail.com>
2025-12-20 10:32:52 +0300

    Refactor Header component to remove AppMenu and Logo, and integrate ThemeTogglerButton for improved theme switching functionality.

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,13 +1,9 @@
-import { AppMenu } from "../shared/app-menu";
-import { Logo } from "../shared/logo";
+import { ThemeTogglerButton } from "../animate-ui/components/buttons/theme-toggler";
 
 export function Header() {
   return (
-    <header className="bg-background/50 backdrop-blur-md shadow sticky top-0 left-0 border-b z-20">
-      <div className="h-16 px-4 container mx-auto flex flex-row gap-4 justify-between items-center">
-        <Logo width="28" height="28"/>
-        <AppMenu />
-      </div>
+    <header>
+      <ThemeTogglerButton modes={["dark"]} />
     </header>
   );
 }
