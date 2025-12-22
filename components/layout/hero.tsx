d6b669779e13a9354e26259f897ae059d3cf757c
Konstantin <altwebga@gmail.com>
2025-12-22 09:53:00 +0300

    Update package dependencies and enhance global styles for improved design consistency. Refactor Hero component layout to better engage users, including new image integration and updated text presentation. Add new utility classes for typography and spacing.

--- components/layout/hero.tsx
+++ components/layout/hero.tsx
@@ -2,26 +2,47 @@ import { BackgroundImageTexture } from "../ui/background-image";
 import { Button } from "../ui/button";
 import { RainbowButton } from "../ui/rainbow-button";
 import { TextEffect } from "../ui/text-effect";
+import Image from "next/image";
 
 export function Hero() {
   return (
     <section className="h-[80vh]">
       <BackgroundImageTexture
         variant="debut-light"
-        opacity={0.2}
-        className="h-[80vh]"
+        opacity={0.4}
+        className="h-[80vh] flex flex-col justify-center"
       >
-        <div className="container mx-auto px-4 space-y-4">
-          <h1>text</h1>
-          <TextEffect>
-            Создаем эффективные сайты, запускаем SEO и рекламу, настраиваем
-            аналитику и помогаем бизнесу расти. Работаем на результат — если не
-            понравится, вернем деньги.
-          </TextEffect>
-          <RainbowButton variant={"outline"} className="hover:translate-2">
-            Rainbow Button
-          </RainbowButton>
-          <Button>acascac</Button>
+        <div className="container mx-auto px-4 flex flex-col md:flex-row">
+          <div className="w-1/3 hidden md:flex justify-center">
+            <Image
+              src={"/img/hero_image.png"}
+              alt="hero image"
+              width={350}
+              height={400}
+              className="w-auto object-cover"
+            />
+          </div>
+          <div className="md:w-2/3 space-y-4 flex flex-col justify-center">
+            <h1 className="flex flex-col md:text-4xl">
+              Разработка и продвижение сайтов{" "}
+              <span className="text-5xl md:text-8xl font-extrabold">
+                в Краснодаре
+              </span>
+            </h1>
+            <TextEffect className="max-w-2xl">
+              Создаем эффективные сайты, запускаем SEO и рекламу, настраиваем
+              аналитику и помогаем бизнесу расти. Работаем на результат — если
+              не понравится, вернем деньги.
+            </TextEffect>
+            <div className="flex gap-4">
+              <RainbowButton className="hover:translate-1">
+                Начать проект
+              </RainbowButton>
+              <Button variant={"outline"} className="hover:translate-1">
+                Приметры работ
+              </Button>
+            </div>
+          </div>
         </div>
       </BackgroundImageTexture>
     </section>
