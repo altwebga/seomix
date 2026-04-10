7e8d9cc8c36217b33c4626944b9d723a5dff129e
Konstantin <altwebga@gmail.com>
2026-04-10 13:28:32 +0300

    feat: add conract Co-authored-by: seomix-ru <275039991+seomix-ru@users.noreply.github.com>

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import { PageHeading } from "@/components/shared/page-heading";
+
+export default function ContactPage() {
+  return (
+    <div className="container mx-auto px-4 my-8">
+      <PageHeading
+        title="Контакты"
+        description="Контакты"
+        hue={180}
+        size={60}
+      />
+    </div>
+  );
+}
